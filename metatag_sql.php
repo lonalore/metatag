7e0a2e0ecee2ea68720fa4fb720ceb34e2570b88
lonalore <lonalore@freemail.hu>
2016-10-14 14:03:02 +0200

    Added DB tables.

---
+++ metatag_sql.php
@@ -0,0 +1,15 @@
+CREATE TABLE `metatag` (
+`entity_id` int(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'The entity id this data is attached to.',
+`entity_type` varchar(50) NOT NULL DEFAULT '' COMMENT 'The entity type this data is attached to.',
+`data` longblob NOT NULL,
+KEY `id` (`entity_id`),
+KEY `type` (`entity_type`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `metatag_default` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Primary ID.',
+`type` varchar(50) NOT NULL DEFAULT '' COMMENT 'The entity type this data is attached to.',
+`data` longblob NOT NULL,
+PRIMARY KEY (`id`),
+KEY `type` (`type`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;

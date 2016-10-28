e09e9abc15b62e4079a226f88d024b3dfb121bc9
lonalore <lonalore@freemail.hu>
2016-10-28 10:40:21 +0200

    Summary for plugin.

--- languages/English/English_global.php
+++ languages/English/English_global.php
@@ -8,6 +8,7 @@
 define("LAN_PLUGIN_METATAG_NAME", "Metatag");
 define("LAN_PLUGIN_METATAG_DESCRIPTION", "This plugin allows you to automatically provide structured metadata, aka meta tags, about a website.");
 define("LAN_PLUGIN_METATAG_TAB", "Metatag");
+define("LAN_PLUGIN_METATAG_SUMMARY", "This plugin allows you to automatically provide structured metadata, aka meta tags, about a website.");
 
 define("LAN_PLUGIN_METATAG_MSG_01", "Meta tags have been updated successfully.");
 
